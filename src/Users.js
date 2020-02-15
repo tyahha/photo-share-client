328b4268a3df4127de48193e30a9127f5512c986
toshinori sato <tyahhahha@gmail.com>
2020-02-15 11:56:33 +0900

    add fake user

--- src/Users.js
+++ src/Users.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { Query } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
+import { gql } from "apollo-boost";
 import { ROOT_QUERY } from "./App";
+
+const ADD_FAKE_USERS_MUTATION = gql`
+  mutation addFakeUsers($count: Int!) {
+    addFakeUsers(count: $count) {
+      githubLogin
+      name
+      avatar
+    }
+  }
+`;
 
 const Users = () => (
   <Query query={ROOT_QUERY}>
@@ -22,6 +33,13 @@ const UserList = ({ count, users, refetchUsers }) => (
   <div>
     <p>{count} Users</p>
     <button onClick={() => refetchUsers()}>Refetch Users</button>
+    <Mutation
+      mutation={ADD_FAKE_USERS_MUTATION}
+      variables={{ count: 1 }}
+      refetchQueries={[{ query: ROOT_QUERY }]}
+    >
+      {addFakeUsers => <button onClick={addFakeUsers}>Add Fake Users</button>}
+    </Mutation>
     <ul>
       {users.map(user => (
         <UserListItem
