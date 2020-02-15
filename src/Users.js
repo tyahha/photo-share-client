6976d109edb4d0cbbe4fe81c78a52945466b63ce
toshinori sato <tyahhahha@gmail.com>
2020-02-15 10:46:25 +0900

    chage gql client graphql-request to apollo-client

---
+++ src/Users.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Query } from "react-apollo";
+import { ROOT_QUERY } from "./App";
+
+const Users = () => (
+  <Query query={ROOT_QUERY}>
+    {result => <p>Users are loading: {result.loading ? "yes" : "no"}</p>}
+  </Query>
+);
+
+export default Users;
--- src/index.js
+++ src/index.js
@@ -1,58 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
+import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { request } from "graphql-request";
+import { ApolloProvider } from "react-apollo";
+import ApolloClient from "apollo-boost";
 
-const url = "http://localhost:4000/graphql";
+const client = new ApolloClient({ uri: "http://localhost:4000/graphql" });
 
-const query = `
-  query listUsers {
-    allUsers {
-      avatar
-      name
-    }
-  }
-`;
-
-const mutation = `
-  mutation populate($count: Int!) {
-    addFakeUsers(count: $count) {
-      githubLogin
-    }
-  }
-`;
-
-function App({ users = [] }) {
-  return (
-    <div>
-      {users.map(user => (
-        <div key={user.githubLogin}>
-          <img src={user.avatar} alt="" />
-          {user.name}
-        </div>
-      ))}
-      <button onClick={addUser}>Add User</button>
-    </div>
-  );
-}
-
-const addUser = () =>
-  request(url, mutation, { count: 1 })
-    .then(requestAndRender)
-    .catch(console.error);
-
-const requestAndRender = () =>
-  request(url, query)
-    .then(render)
-    .catch(console.error);
-
-export default App;
-
-const render = ({ allUsers = [] }) =>
-  ReactDOM.render(<App users={allUsers} />, document.getElementById("root"));
-
-requestAndRender();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
