b2ac0172fd0b66f75d6648bd73625b038c7eafd3
toshinori sato <tyahhahha@gmail.com>
2020-02-15 21:54:26 +0900

    implement Me Component

--- src/AuthorizedUser.js
+++ src/AuthorizedUser.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router";
-import { Mutation } from "react-apollo";
+import { Query, Mutation } from "react-apollo";
 import { gql } from "apollo-boost";
 import { ROOT_QUERY } from "./App";
 
@@ -17,6 +17,31 @@ const requestCode = () => {
   window.location = `https://github.com/login/oauth/authorize?client_id=${clinentID}&scope=user`;
 };
 
+const Me = ({ logout, requestCode, signingIn }) => (
+  <Query query={ROOT_QUERY}>
+    {q => {
+      const { loading, data } = q;
+      return data && data.me ? (
+        <CurrentUser {...data.me} logout={logout} />
+      ) : loading ? (
+        <p>loading... </p>
+      ) : (
+        <button onClick={requestCode} disabled={signingIn}>
+          Sign In with Github
+        </button>
+      );
+    }}
+  </Query>
+);
+
+const CurrentUser = ({ name, avatar, logout }) => (
+  <div>
+    <img src={avatar} width={48} height={48} alt="" />
+    <h1>{name}</h1>
+    <button onClick={logout}>logout</button>
+  </div>
+);
+
 const Inner = ({ githubAuthMutation, signingIn, setSigningIn }) => {
   useEffect(() => {
     if (window.location.search.match(/code=/)) {
@@ -27,9 +52,11 @@ const Inner = ({ githubAuthMutation, signingIn, setSigningIn }) => {
   }, [githubAuthMutation, setSigningIn]);
 
   return (
-    <button onClick={requestCode} disabled={signingIn}>
-      Sign In with Github
-    </button>
+    <Me
+      signingIn={signingIn}
+      requestCode={requestCode}
+      logout={() => localStorage.removeItem("token")}
+    />
   );
 };
 
